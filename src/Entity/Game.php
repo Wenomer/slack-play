3792c29d7296560b6e5a31516dcaea3b7367ea5e
Alex Schastiy <alexey.schastniy@weavora.com>
2018-11-27 15:16:46 +0300

    little fixes

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -16,6 +17,7 @@ class Game
     public function __construct()
     {
         $this->createdAt = new \DateTime('now');
+        $this->players = new ArrayCollection();
     }
 
     /**
